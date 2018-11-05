b9660a9691d5663bf0843d04ad94458daba34376
Miguel Triana <mikesoft241184@gmail.com>
2018-11-05 14:17:51 -0500

    src/entries/home.js

--- src/entries/home.js
+++ src/entries/home.js
@@ -7,7 +7,7 @@ import Home from "../pages/containers/home";
 
 import { Provider } from 'react-redux';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from '../reducers/index'
 import { Map as map } from 'immutable';
@@ -29,10 +29,31 @@ import { Map as map } from 'immutable';
     }
 };*/
 
+/*function logger({ getState, dispath}){
+   return (next)=>{
+      return (action) => {
+         console.log('estae es mi viejo estado', getState().toJS());
+         console.log('vamos a enviar esta aciion', action);
+         const value =  next(action);
+         console.log('estae es mi nuevo estado', getState().toJS());
+         return value;
+      }
+   }
+}*/
+
+const logger = ({ getState, dispath}) => next => action => {
+    console.log('estae es mi viejo estado', getState().toJS());
+    console.log('vamos a enviar esta aciion', action);
+    const value =  next(action);
+    console.log('estae es mi nuevo estado', getState().toJS());
+    return value;
+};
+
 const store = createStore(
     reducer,
     map(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(logger)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 //console.log(store.getState())
